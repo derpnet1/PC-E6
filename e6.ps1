d07fb49d6997b3e622bd92b8de532657be52f2c4
derpnet1 <89653100+derpnet1@users.noreply.github.com>
2021-10-04 18:06:25 -0500

    Add files via upload

---
+++ e6.ps1
@@ -0,0 +1,40 @@
+﻿#Daniel Ernesto Rangel Perez
+#Esteban Osorio Rodriguez
+#El objetivo del script es crear, mover y elimar carpetas.
+
+Import-Module Tarea\modulo2.psm1
+New-ModuleManifest -Path 'C:\Program Files\WindowsPowerShell\Modules\Tarea\manifiesto2.psd1' -Author "Wi-FiBotz" -RootModule modulo2 -Description "Manifiesto de tarea E6"
+
+$a = 0
+while($a -eq 0){
+ $opc = Read-Host "
+ [1]-Crear Una Carpeta
+ [2]-Mover Una Carpeta
+ [3]-Eliminar Una Carpeta
+ [0]Salir
+
+ Elige una opcion"
+ 
+  switch($opc){
+    1{
+        crearCarpeta 
+        Write-Host "*********************************************"
+        break
+    }
+    
+    2{
+        moverCarpeta 
+        Write-Host "*********************************************"
+        break
+    }
+    3{
+        borrarCarpeta
+        Write-Host "*********************************************"
+        break
+    }
+    0{
+        $a = 1
+        Write-Host "Adios =)"
+    }
+  }  
+}
---
+++ modulo2.psm1
@@ -0,0 +1,24 @@
+﻿
+function crearCarpeta(){
+    
+    $name = read-host "Escribe el nombre de la carpeta "
+    $fnpath = read-host "Escribe la ruta donde quieres crear la carpeta "
+    New-Item $fnpath\$name  
+}
+
+
+function moverCarpeta(){
+    
+    
+    $test = read-host "Escribe la ruta donde se encuentra la carpeta "
+    $test1 = Read-Host "Escribe la nueva ruta de la carpeta "
+    Move-Item $test $test1 -errorAction continue
+}
+
+
+function borrarCarpeta(){
+    
+    $fpath = read-host "Escriba la ruta de la carpeta a borrar "
+    Remove-item $fpath -ErrorAction Continue
+
+}
